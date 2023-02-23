eb8413fa8bf8c9e6d3ba00327b098240d04cb861
hanqunfeng <hanqf2008@163.com>
2023-02-23 17:57:42 +0800

    Site updated: 2023-02-23 17:57:41

--- js-expand/httpToHttps.js
+++ js-expand/httpToHttps.js
@@ -1,5 +1,6 @@
 
 var reg = /^(10.10.2.)/;
+// 判断非本地server时 http强制转换成https
 if (window.location.hostname !== 'localhost' && !reg.test(window.location.hostname)) {
     if (window.location.protocol === 'http:') {
         window.location.href = window.location.href.replace('http:', 'https:')
