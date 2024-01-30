e2f313daa0234fa308db10f42e3c20b9628784dd
hanqunfeng <hanqf2008@163.com>
2024-01-30 18:02:55 +0800

    Site updated: 2024-01-30 18:02:54

--- js-expand/baidu_tongji.js
+++
@@ -1,9 +0,0 @@
-
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?e5cd6c30fc944bddf6ed6ae96edc722b";
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(hm, s);
-})();
-
